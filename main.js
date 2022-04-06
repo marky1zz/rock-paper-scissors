bd191e644218730ca6d2e6a11a3afbc5a84a96e0
Martinas Menčiūnas <mart1z5916@gmail.com>
2022-04-07 02:55:11 +0300

    made an UI

--- main.js
+++ main.js
@@ -1,87 +1,91 @@
-let playerPick = 0;
-let computerPick = 0;
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const playerScoreSpan = document.querySelector('#player');
+const computerScoreSpan = document.querySelector('#computer');
+const text = document.querySelector('#text');
 
-let game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    while (playerScore  < 5 && computerScore < 5) {
-        let getPicks = () => {
-            playerPick = prompt("1. Rock 2. Paper 3.Scissors");
-            computerPick = Math.floor(Math.random() * 3) + 1;
-        }
-    
-        getPicks();
-    
-        
-        // player pics    
-        if (playerPick == 1) {
-            playerPick = "rock";
-        }
-        else if (playerPick == 2) {
-            playerPick = "paper";
-        }
-        else if (playerPick == 3) {
-            playerPick = "scissors";
-        }
-        // computer picks
-        if (computerPick == 1) {
-            computerPick = "rock";
-        }
-        else if (computerPick == 2) {
-            computerPick = "paper";
-        }
-        else if (computerPick == 3) {
-            computerPick = "scissors";
-        }
-        
-        console.log("player picked: " + playerPick);
-        console.log("computer picked: " + computerPick);
-        
-        // checking the winner
-        if (playerPick == "rock" && computerPick == "scissors") {
-            console.log("Player wins");
-            playerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == "rock" && computerPick == "paper") {
-            console.log("Computer wins");
-            playerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == "paper" && computerPick == "scissors") {
-            console.log("Computer wins");
-            computerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == "paper" && computerPick == "rock") {
-            console.log("Player wins");
-            playerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == "scissors" && computerPick == "paper") {
-            console.log("Player wins")
-            playerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == "scissors" && computerPick == "rock") {
-            console.log("Computer wins");
-            computerScore++;
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-        else if (playerPick == computerPick) {
-            console.log("DRAW");
-            console.log(`Player: ${playerScore} computer: ${computerScore}`);
-        }
-
-        
-    }
-    playerScore == 5 ? console.log("PLAYER WON") : console.log("COMPUTER WON");
-    
-    
+let getComputerChoice = () => {
+    const choices = ['r', 'p', 's'];
+    randomNumber = Math.floor(Math.random() * 3);
+    computerPick = choices[randomNumber];
+    console.log('Computer picked: ' + computerPick);
+    return computerPick;
 }
 
-game();
+let getWinner = () => {
+    if (playerScore == 5) {
+        document.body.style.backgroundColor = 'green';
+        text.innerHTML = 'Player won the game';
+        rockBtn.style.pointerEvents = 'none';
+        paperBtn.style.pointerEvents = 'none';
+        scissorsBtn.style.pointerEvents = 'none';
+
+    }
+    else if (computerScore == 5) {
+        document.body.style.backgroundColor = 'red';
+        text.innerHTML = 'Comp won the game';
+        rockBtn.style.pointerEvents = 'none';
+        paperBtn.style.pointerEvents = 'none';
+        scissorsBtn.style.pointerEvents = 'none';
+
+    }
+}
+
+let checkRound = (P, COMP) => {
+    switch (P + COMP) {
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            console.log('DRAW!!!');
+            text.innerHTML = 'DRAW!!!';
+            break;
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            text.innerHTML = 'Player wins!!!';
+            playerScore++
+            playerScoreSpan.innerHTML = playerScore;
+            break;
+        case 'sr':
+        case 'rp':
+        case 'ps':
+            text.innerHTML = 'COMP wins!!!';
+            computerScore++;
+            computerScoreSpan.innerHTML = computerScore;
+            break;
+        }
+        getWinner();
+}
+
+let game = (playerChoice) => {
+    console.log('Player picked: ' + playerChoice);
+    computerChoice = getComputerChoice();
+    checkRound(playerChoice, computerChoice);
+}
+
+rockBtn.addEventListener('click', () => {
+    game('r');    
+});
+paperBtn.addEventListener('click', () => {
+    game('p');    
+});
+scissorsBtn.addEventListener('click', () => {
+    game('s');    
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
